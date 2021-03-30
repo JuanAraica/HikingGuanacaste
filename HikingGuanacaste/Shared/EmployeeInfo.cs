38994fbef2c0d5158e6e6d0a1dee00a6dfb5216d
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2021-03-29 19:32:56 -0600

    Agregar archivos de proyecto.

---
+++ HikingGuanacaste/Shared/EmployeeInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikingGuanacaste.Shared
+{
+    public class EmployeeInfo
+    {
+        public string Empid { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+    }
+}
